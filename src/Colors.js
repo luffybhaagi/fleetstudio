e6ef228a5de9d53634184f2688cfbeaed29cc0ea
bhagath reddy <bhagath.reddy@oracle.com>
2020-11-14 12:23:44 +0530

    fleet studio task

---
+++ src/Colors.js
@@ -0,0 +1,30 @@
+import {purple, red, cyan, blue, deepPurple, grey, pink, indigo, orange, brown,amber,lightBlue,yellow} from '@material-ui/core/colors';
+
+export const Colors={
+    'A':amber[500], //amber
+    'B':blue[500], //blue
+    'C':cyan[500],//cyan
+    'D':blue[900],//deep blue
+    'E':red[200],//red[A200]
+    'F':red[900],//red[900]
+    'G':grey[500],//grey
+    'H':pink[500],//pink[200]
+    'I':indigo[500],//indigo
+    'J':pink[900],//pink[A700]
+    'K':purple[200],//purple[200]
+    'L':lightBlue[500],//lightblue
+    'M':purple[900],//purple[A700]
+    'N':deepPurple[200],//deeppurple[200]
+    'O':orange[500],//orange
+    'P':purple[500],//purple
+    'Q':deepPurple[700],//dpurple[700]
+    'R':red[500],//red
+    'S':indigo[200],//indigo[200]
+    'T':indigo[900],//indigo[A700]
+    'U':blue[200],//blue[200]
+    'V':brown[900],//blue[A700]
+    'W':yellow[200],//bluegray[200]
+    'X':brown[500],//brown
+    'Y':yellow[900],//yellow
+    'Z':yellow[500],//grey[900]
+}
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,99 +1,176 @@
-import { Box, Button, ButtonGroup, Container, Paper, TextField, Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import React from 'react'
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Container,
+  Divider,
+  Paper,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import cuid from "cuid";
+import React, { useState } from "react";
+import { Colors } from "./Colors";
+import Snackbar from "@material-ui/core/Snackbar";
+import Alert from "@material-ui/lab/Alert";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',     
-      '& > *': {
-        margin: theme.spacing(1),
-      },
-    },
-    container2:{
-        display: 'flex',
-      flexDirection: 'row',
-      alignItems: 'center',     
-      '& > *': {
-        margin: theme.spacing(1),
-      },
-    },
-    serverButton:{
-        '& > *':{
-            margin: theme.spacing(1),
-        }
-    },
-    tasks:{
-        display:'flex',
-        flexDirection:'row',
-        alignItems: 'center',
-    },
-    queue:{
-        '& > *':{
-            margin: theme.spacing(2),
-        },
-        backgroundColor:"lightBlue",
-        display:'flex',
-        flexDirection:'row',
-        alignItems: 'center',
-    },
-    container3: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',     
-        '& > *': {
-          margin: theme.spacing(1),
-        },
-      },
-      color:{
-          background:'blue'
+const ColorsMap = Colors;
+
+function HomePage() {
+
+  const classes = useStyles();
+  const [text, setText] = useState(null);
+  const [colors, setColors] = useState([]);
+  const [alert, setAlert] = useState(false);
+
+  const handleSubmit = () => {
+    if (text === null || text === "") {
+      setAlert(true);
+    } else {
+      let localColorsArray = [];
+      //removes duplicate chars from string
+      const modText=removeDuplicateCharacters(text)
+    //storing colors
+      for (let i = 0; i < modText.length; i++) {
+        console.log(ColorsMap[modText.charAt(i).toUpperCase()]);
+        localColorsArray.push(ColorsMap[modText.charAt(i).toUpperCase()]);
       }
-  }));
+      setColors(localColorsArray);
+    }
+  };
 
-function HomePage(){
+  function removeDuplicateCharacters(string) {
+    return string
+      .split('')
+      .filter(function(item, pos, self) {
+        return self.indexOf(item) === pos;
+      })
+      .join('');
+  }
 
-    const classes = useStyles();
-    const colors =[
-        "#FFFFFF",
-        "#000000",
-        "#000080",
-        "#6495ed",
-        "#483d8b",
-        "#6a5acd",
-    ]
-    // const data = [];
-    // data.push(colors)
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
 
-    return(
-        <>
-         <Container className={classes.root}>
-          <Paper className={classes.container2} >
-           {/* <Paper className={classes.serverButton}> */}
-            <TextField 
-             id="outlined-basic" 
-             label="Search name" 
-             variant="outlined"
-             size="small" 
-              />
-           {/* </Paper> */}
-           {/* <Paper className={classes.queue}> */}
-           <ButtonGroup variant="contained" color="secondary" aria-label="contained primary button group">
-                <Button>Submit</Button>
-                <Button>Clear</Button>
-            </ButtonGroup>
-           {/* </Paper> */}
-           </Paper>
-       </Container>
-       <Paper className={classes.container3} square >
-           {colors.map()((color)=>(
-               <Box colors={colors} />
-           ))}
-           <Box color="#000000" />
+  const handleClear = () => {
+    setText("");
+    setColors([]);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setAlert(false);
+  };
+
+  return (
+    <>
+      <Container className={classes.root}>
+      <Typography variant="h2" gutterBottom style={{fontFamily:"cursive"}}>
+        Colo`extism
+      </Typography>
+        <Paper sm={3} className={classes.container2}>
+          <TextField
+            id="outlined-basic"
+            label="Enter Word"
+            variant="outlined"
+            value={text}
+            size="small"
+            onChange={(event) => handleChange(event)}
+          />
+          <ButtonGroup
+            variant="contained"
+            color="secondary"
+            aria-label="contained primary button group"
+          >
+            <Button onClick={handleSubmit}>Submit</Button>
+            <Button onClick={handleClear}>Clear</Button>
+          </ButtonGroup>
         </Paper>
-   </>
-    )
+      </Container>
+      <br></br>
+      <Divider />
+      <br></br>
+      <Container fixed className={classes.container3} square>
+        {colors.map((color) => (
+          <Box
+            className={classes.colorBox}
+            key={cuid()}
+            style={{ backgroundColor: `${color}` }}
+          >
+            <Typography>{color}</Typography>
+          </Box>
+        ))}
+      </Container>
+      {alert && (
+        <Snackbar open={alert} autoHideDuration={2000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="error">
+            Enter text to see colors
+          </Alert>
+        </Snackbar>
+      )}
+    </>
+  );
 }
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  container2: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+    marginTop:'2%'
+  },
+  serverButton: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  tasks: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  queue: {
+    "& > *": {
+      margin: theme.spacing(2),
+    },
+    backgroundColor: "lightBlue",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  container3: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "center",
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  color: {
+    background: "blue",
+  },
+  colorBox: {
+    width: "20%",
+    minHeight: "200px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+}));
 
 export default HomePage;
